6fdeda9ae32a7fa411630563a528bd3daa8abcc3
unknown <urs.lang@gmail.com>
2021-04-28 22:02:22 +0200

    .

--- src/Style/Style.php
+++ src/Style/Style.php
@@ -3,12 +3,27 @@
 namespace Nemundo\Geo\Kml\Style;
 
 
+use Nemundo\Html\Container\AbstractContainer;
 use Nemundo\Html\Container\AbstractTagContainer;
 
+// KmlStyle
 class Style extends AbstractTagContainer
 {
 
     public $styleId;
+
+
+    protected function loadStyle() {
+
+    }
+
+
+    public function __construct(AbstractContainer $parentContainer = null)
+    {
+        parent::__construct($parentContainer);
+        $this->loadStyle();
+    }
+
 
     public function getContent()
     {
--- tag.bat
+++ tag.bat
@@ -1,5 +1,5 @@
 git add *
 git commit -m "."
 git push
-git tag 0.5
+git tag 0.6
 git push --tags
